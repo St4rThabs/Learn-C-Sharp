db0e4580f8b40ccc258933d2657eb8d1e9ef6a49
thabata.lima TL. <thabata.lima@tecdetbrg.com.br>
2024-05-13 14:55:08 -0300

    Implementação do HashSet e SortedHash

---
+++ NovoBanco/NovoBanco/Busca/GeradorDeDevedores.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoBanco.Busca
+{
+    class GeradorDeDevedores
+    {
+        public SortedSet<string> GeraList()
+        {
+            SortedSet<string> nomes = new SortedSet<string>();
+            for (int i = 0; i < 30000; i++)
+            {
+                nomes.Add("devedor " + i);
+            }
+            return nomes;
+        }
+    }
+}
--- NovoBanco/NovoBanco/FormCadastroConta.cs
+++ NovoBanco/NovoBanco/FormCadastroConta.cs
@@ -8,11 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using NovoBanco.Contas;
+using NovoBanco.Busca;
 
 namespace NovoBanco
 {
     public partial class FormCadastroConta : Form
     {
+        private ICollection<string> devedores;
         // modificando o construtor da classe FromCadastroConta para receber as instância do Form1
         private Form1 formPrincipal;
         private Conta novaConta;
@@ -25,6 +27,9 @@ namespace NovoBanco
             comboTipoConta.Items.Add("Conta Corrente");
             comboTipoConta.Items.Add("Conta Poupança");
             comboTipoConta.Items.Add("Conta Investimento");
+
+            GeradorDeDevedores gerador = new GeradorDeDevedores();
+            this.devedores = gerador.GeraList();
         }
 
         public FormCadastroConta()
@@ -37,29 +42,54 @@ namespace NovoBanco
             // cadastrando uma nova instância de conta
             int indice = comboTipoConta.SelectedIndex;
 
+            // verificando se o titular inserido é devedor ou não
+            string titular = textoTitular.Text;
+            bool ehDevedor = this.devedores.Contains(titular);
+
             // com o switch case verifica qual o tipo de conta que selecionou para criar uma nova conta
             switch (indice)
             {
                 case 0:
-                    this.novaConta = new ContaCorrente();
-                    novaConta.Titular = new Cliente(textoTitularCadastro.Text);
-                    textoNumero.Text = Convert.ToString(Conta.ProximoNumero()); // -> a conta agora cria o próprio número, então não precisa mais do textoNumero.Text, basta utilizar o método estático
+                    if (!ehDevedor)
+                    {
+                        this.novaConta = new ContaCorrente();
+                        novaConta.Titular = new Cliente(textoTitularCadastro.Text);
+                        textoNumero.Text = Convert.ToString(Conta.ProximoNumero()); // -> a conta agora cria o próprio número, então não precisa mais do textoNumero.Text, basta utilizar o método estático
 
-                    this.formPrincipal.AdicionaConta(novaConta);
+                        this.formPrincipal.AdicionaConta(novaConta);
+                    } else
+                    {
+                        MessageBox.Show("devedor");
+                    }
+
                     break;
                 case 1:
-                    this.novaConta = new ContaPoupanca();
-                    novaConta.Titular = new Cliente(textoTitularCadastro.Text);
-                    textoNumero.Text = Convert.ToString(Conta.ProximoNumero()); 
+                    if (!ehDevedor)
+                    {
+                        this.novaConta = new ContaInvestimento();
+                        novaConta.Titular = new Cliente(textoTitularCadastro.Text);
+                        textoNumero.Text = Convert.ToString(Conta.ProximoNumero()); // -> a conta agora cria o próprio número, então não precisa mais do textoNumero.Text, basta utilizar o método estático
 
-                    this.formPrincipal.AdicionaConta(novaConta);
+                        this.formPrincipal.AdicionaConta(novaConta);
+                    }
+                    else
+                    {
+                        MessageBox.Show("devedor");
+                    }
                     break;
                 case 2:
-                    this.novaConta = new ContaInvestimento();
-                    novaConta.Titular = new Cliente(textoTitularCadastro.Text);
-                    textoNumero.Text = Convert.ToString(Conta.ProximoNumero());
+                    if (!ehDevedor)
+                    {
+                        this.novaConta = new ContaInvestimento();
+                        novaConta.Titular = new Cliente(textoTitularCadastro.Text);
+                        textoNumero.Text = Convert.ToString(Conta.ProximoNumero()); // -> a conta agora cria o próprio número, então não precisa mais do textoNumero.Text, basta utilizar o método estático
 
-                    this.formPrincipal.AdicionaConta(novaConta);
+                        this.formPrincipal.AdicionaConta(novaConta);
+                    }
+                    else
+                    {
+                        MessageBox.Show("devedor");
+                    }
                     break;
                 default:
                     MessageBox.Show("Selecione o tipo de conta");
