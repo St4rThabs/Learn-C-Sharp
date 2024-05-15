a87d20bd63df95120dea3cbf9d6d8f9185e2bcf8
thabata.lima TL. <thabata.lima@tecdetbrg.com.br>
2024-05-15 15:05:35 -0300

    Implementação de busca utilizando queries

--- NovoBanco/NovoBanco/FormRelatorios.Designer.cs
+++ NovoBanco/NovoBanco/FormRelatorios.Designer.cs
@@ -32,7 +32,13 @@ namespace NovoBanco
             this.listaResultado = new System.Windows.Forms.ListBox();
             this.botaoFiltroSaldo = new System.Windows.Forms.Button();
             this.Resumo = new System.Windows.Forms.GroupBox();
+            this.labelMaiorSaldo = new System.Windows.Forms.Label();
+            this.labelSaldoTotal = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.botaoAntigas = new System.Windows.Forms.Button();
+            this.botaoMedia = new System.Windows.Forms.Button();
+            this.Resumo.SuspendLayout();
             this.SuspendLayout();
             // 
             // listaResultado
@@ -41,7 +47,7 @@ namespace NovoBanco
             this.listaResultado.FormattingEnabled = true;
             this.listaResultado.Location = new System.Drawing.Point(12, 12);
             this.listaResultado.Name = "listaResultado";
-            this.listaResultado.Size = new System.Drawing.Size(440, 134);
+            this.listaResultado.Size = new System.Drawing.Size(440, 160);
             this.listaResultado.TabIndex = 0;
             // 
             // botaoFiltroSaldo
@@ -53,7 +59,7 @@ namespace NovoBanco
             this.botaoFiltroSaldo.ForeColor = System.Drawing.Color.Transparent;
             this.botaoFiltroSaldo.Location = new System.Drawing.Point(12, 399);
             this.botaoFiltroSaldo.Name = "botaoFiltroSaldo";
-            this.botaoFiltroSaldo.Size = new System.Drawing.Size(120, 30);
+            this.botaoFiltroSaldo.Size = new System.Drawing.Size(110, 30);
             this.botaoFiltroSaldo.TabIndex = 5;
             this.botaoFiltroSaldo.Text = "Saldo > 5000";
             this.botaoFiltroSaldo.UseVisualStyleBackColor = false;
@@ -62,12 +68,56 @@ namespace NovoBanco
             // Resumo
             // 
             this.Resumo.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.Resumo.Location = new System.Drawing.Point(12, 161);
+            this.Resumo.Controls.Add(this.labelMaiorSaldo);
+            this.Resumo.Controls.Add(this.labelSaldoTotal);
+            this.Resumo.Controls.Add(this.label2);
+            this.Resumo.Controls.Add(this.label1);
+            this.Resumo.Location = new System.Drawing.Point(12, 196);
             this.Resumo.Name = "Resumo";
-            this.Resumo.Size = new System.Drawing.Size(440, 232);
+            this.Resumo.Size = new System.Drawing.Size(440, 183);
             this.Resumo.TabIndex = 6;
             this.Resumo.TabStop = false;
             this.Resumo.Text = "Resumo";
+            // 
+            // labelMaiorSaldo
+            // 
+            this.labelMaiorSaldo.AutoSize = true;
+            this.labelMaiorSaldo.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold);
+            this.labelMaiorSaldo.Location = new System.Drawing.Point(251, 119);
+            this.labelMaiorSaldo.Name = "labelMaiorSaldo";
+            this.labelMaiorSaldo.Size = new System.Drawing.Size(121, 19);
+            this.labelMaiorSaldo.TabIndex = 12;
+            this.labelMaiorSaldo.Text = "labelMaiorSaldo";
+            // 
+            // labelSaldoTotal
+            // 
+            this.labelSaldoTotal.AutoSize = true;
+            this.labelSaldoTotal.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold);
+            this.labelSaldoTotal.Location = new System.Drawing.Point(251, 48);
+            this.labelSaldoTotal.Name = "labelSaldoTotal";
+            this.labelSaldoTotal.Size = new System.Drawing.Size(116, 19);
+            this.labelSaldoTotal.TabIndex = 11;
+            this.labelSaldoTotal.Text = "labelSaldoTotal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(41, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 19);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Maior Saldo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(41, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 19);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Saldo Total";
             // 
             // botaoAntigas
             // 
@@ -76,13 +126,28 @@ namespace NovoBanco
             this.botaoAntigas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.botaoAntigas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.botaoAntigas.ForeColor = System.Drawing.Color.Transparent;
-            this.botaoAntigas.Location = new System.Drawing.Point(332, 399);
+            this.botaoAntigas.Location = new System.Drawing.Point(342, 399);
             this.botaoAntigas.Name = "botaoAntigas";
-            this.botaoAntigas.Size = new System.Drawing.Size(120, 30);
+            this.botaoAntigas.Size = new System.Drawing.Size(110, 30);
             this.botaoAntigas.TabIndex = 7;
             this.botaoAntigas.Text = "Antigas";
             this.botaoAntigas.UseVisualStyleBackColor = false;
             this.botaoAntigas.Click += new System.EventHandler(this.botaoAntigas_Click);
+            // 
+            // botaoMedia
+            // 
+            this.botaoMedia.BackColor = System.Drawing.Color.CadetBlue;
+            this.botaoMedia.FlatAppearance.MouseOverBackColor = System.Drawing.Color.PowderBlue;
+            this.botaoMedia.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botaoMedia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoMedia.ForeColor = System.Drawing.Color.Transparent;
+            this.botaoMedia.Location = new System.Drawing.Point(177, 399);
+            this.botaoMedia.Name = "botaoMedia";
+            this.botaoMedia.Size = new System.Drawing.Size(110, 30);
+            this.botaoMedia.TabIndex = 8;
+            this.botaoMedia.Text = "Média";
+            this.botaoMedia.UseVisualStyleBackColor = false;
+            this.botaoMedia.Click += new System.EventHandler(this.botaoMedia_Click);
             // 
             // FormRelatorios
             // 
@@ -90,12 +155,15 @@ namespace NovoBanco
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateGray;
             this.ClientSize = new System.Drawing.Size(464, 441);
+            this.Controls.Add(this.botaoMedia);
             this.Controls.Add(this.botaoAntigas);
             this.Controls.Add(this.Resumo);
             this.Controls.Add(this.botaoFiltroSaldo);
             this.Controls.Add(this.listaResultado);
             this.Name = "FormRelatorios";
             this.Text = "FormRelatorios";
+            this.Resumo.ResumeLayout(false);
+            this.Resumo.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -106,5 +174,10 @@ namespace NovoBanco
         private System.Windows.Forms.Button botaoFiltroSaldo;
         private System.Windows.Forms.GroupBox Resumo;
         private System.Windows.Forms.Button botaoAntigas;
+        private System.Windows.Forms.Button botaoMedia;
+        private System.Windows.Forms.Label labelMaiorSaldo;
+        private System.Windows.Forms.Label labelSaldoTotal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- NovoBanco/NovoBanco/FormRelatorios.cs
+++ NovoBanco/NovoBanco/FormRelatorios.cs
@@ -26,26 +26,41 @@ namespace NovoBanco
             listaResultado.Items.Clear();
             var maiorQueCincoMil = from c in contas
                                    where c.Saldo > 5000
-                                   select new { c.Titular.Nome, c.Numero, c.Saldo };
+                                   orderby c.Titular.Nome, c.Numero
+                                   select new { c.Titular.Nome, c.Numero, c.Saldo }; // buscando utilizando a sintaxe de queries e devolvendo um objeto anônimo 
 
             foreach (var c in maiorQueCincoMil)
             {
-                listaResultado.Items.Add(c);
+                listaResultado.Items.Add(c); // mostrando o objeto anônimo
             }
         }
 
         private void botaoAntigas_Click(object sender, EventArgs e)
         {
             listaResultado.Items.Clear();
-            var antigas = from c in contas
-                          where c.Saldo > 1000 && c.Numero < 10
-                          select new { c.Titular.Nome, c.Numero, c.Saldo };
+            var antigas = contas.Where(c => c.Saldo > 1000 && c.Numero < 10)
+                                .OrderBy(c => c.Titular.Nome)
+                                .Select(c => new { c.Titular.Nome, c.Numero, c.Saldo });
 
             foreach (var c in antigas)
             {
                 listaResultado.Items.Add(c);
             }
 
+        }
+
+        private void botaoMedia_Click(object sender, EventArgs e)
+        {
+            listaResultado.Items.Clear();
+            var media = contas.Average(c => c.Saldo); // utilizando lambda e o método average do linq para pegar a média de saldos
+
+            listaResultado.Items.Add("Média = " + Math.Round(media, 2));
+
+            var saldoTotal = contas.Sum(c => c.Saldo);
+            var maiorSaldo = contas.Max(c => c.Saldo);
+
+            labelSaldoTotal.Text = Convert.ToString(saldoTotal); // atualizando a label para mostrar o saldo total
+            labelMaiorSaldo.Text = Convert.ToString(maiorSaldo); // atualizando a label para mostrar o maior saldo
         }
     }
 }
