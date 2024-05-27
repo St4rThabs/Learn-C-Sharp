05728838020ab8b2e9b439f08bc66efbc43feeea
thabata.lima TL. <thabata.lima@tecdetbrg.com.br>
2024-05-27 15:00:47 -0300

    Remoção do projeto CadastroPacientes e distribuição do CursoWindowsFormsAlura

---
+++ Alura/WindowsFormsAlura/CursoWindowsFormsAlura/Formulários Curso 1/Frm_ValidaSenha.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CursoWindowsFormsBiblioteca;
+
+namespace CursoWindowsFormsAlura
+{
+    public partial class Frm_ValidaSenha : Form
+    {
+        private bool VerSenha = false;
+
+        public Frm_ValidaSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Txt_Senha.Text = "";
+            Lbl_Resultado.Text = "";
+        }
+
+        private void Txt_Senha_KeyDown(object sender, KeyEventArgs e)
+        {
+            Cls_Uteis.ChecaForcaSenha verifica = new Cls_Uteis.ChecaForcaSenha();
+            Cls_Uteis.ChecaForcaSenha.ForcaDaSenha forca;
+
+            forca = verifica.GetForcaDaSenha(Txt_Senha.Text);
+
+            Lbl_Resultado.Text = forca.ToString();
+
+            if (Lbl_Resultado.Text == "Inaceitavel" || Lbl_Resultado.Text == "Fraca")
+            {
+                Lbl_Resultado.ForeColor = Color.Red;
+            }
+
+            if (Lbl_Resultado.Text == "Aceitavel")
+            {
+                Lbl_Resultado.ForeColor = Color.Blue;
+            }
+            if (Lbl_Resultado.Text == "Forte" || Lbl_Resultado.Text == "Segura")
+            {
+                Lbl_Resultado.ForeColor = Color.DarkOliveGreen;
+            }
+
+        }
+
+        private void Btn_VerSenha_Click(object sender, EventArgs e)
+        {
+            if (VerSenha == false)
+            {
+                Txt_Senha.PasswordChar = '\0'; // o \0 representa um caracter vazio
+                VerSenha = true;
+                Btn_VerSenha.Text = "Ocultar Senha";
+            }
+            else
+            {
+                Txt_Senha.PasswordChar = '*';
+                VerSenha = false;
+                Btn_VerSenha.Text = "Mostrar Senha";
+            }
+
+        }
+    }
+
+}
