012ab37932456106fb54e1daa7caaa7a3514db85
thabata.lima TL. <thabata.lima@tecdetbrg.com.br>
2024-05-29 14:43:45 -0300

    Finalização do projeto ConsumoEnergia

---
+++ SolutionCapitulo01/ConsumoEnergia/FormConsumo.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ConsumoEnergia
+{
+    partial class FormConsumo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvLeituras = new System.Windows.Forms.DataGridView();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtCasa = new System.Windows.Forms.TextBox();
+            this.txtConsumo = new System.Windows.Forms.TextBox();
+            this.lblNCasa = new System.Windows.Forms.Label();
+            this.lblConsumo = new System.Windows.Forms.Label();
+            this.btnProcessar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeituras)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvLeituras
+            // 
+            this.dgvLeituras.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLeituras.Location = new System.Drawing.Point(12, 47);
+            this.dgvLeituras.Name = "dgvLeituras";
+            this.dgvLeituras.Size = new System.Drawing.Size(407, 197);
+            this.dgvLeituras.TabIndex = 0;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(344, 12);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 1;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click_1);
+            // 
+            // txtCasa
+            // 
+            this.txtCasa.Location = new System.Drawing.Point(69, 15);
+            this.txtCasa.Name = "txtCasa";
+            this.txtCasa.Size = new System.Drawing.Size(71, 20);
+            this.txtCasa.TabIndex = 2;
+            // 
+            // txtConsumo
+            // 
+            this.txtConsumo.Location = new System.Drawing.Point(223, 14);
+            this.txtConsumo.Name = "txtConsumo";
+            this.txtConsumo.Size = new System.Drawing.Size(100, 20);
+            this.txtConsumo.TabIndex = 3;
+            // 
+            // lblNCasa
+            // 
+            this.lblNCasa.AutoSize = true;
+            this.lblNCasa.Location = new System.Drawing.Point(12, 17);
+            this.lblNCasa.Name = "lblNCasa";
+            this.lblNCasa.Size = new System.Drawing.Size(51, 13);
+            this.lblNCasa.TabIndex = 4;
+            this.lblNCasa.Text = "Nr. Casa:";
+            // 
+            // lblConsumo
+            // 
+            this.lblConsumo.AutoSize = true;
+            this.lblConsumo.Location = new System.Drawing.Point(163, 17);
+            this.lblConsumo.Name = "lblConsumo";
+            this.lblConsumo.Size = new System.Drawing.Size(54, 13);
+            this.lblConsumo.TabIndex = 5;
+            this.lblConsumo.Text = "Consumo:";
+            // 
+            // btnProcessar
+            // 
+            this.btnProcessar.Location = new System.Drawing.Point(12, 250);
+            this.btnProcessar.Name = "btnProcessar";
+            this.btnProcessar.Size = new System.Drawing.Size(103, 23);
+            this.btnProcessar.TabIndex = 6;
+            this.btnProcessar.Text = "Processar dados";
+            this.btnProcessar.UseVisualStyleBackColor = true;
+            this.btnProcessar.Click += new System.EventHandler(this.btnProcessar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(249, 255);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 7;
+            // 
+            // FormConsumo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 276);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnProcessar);
+            this.Controls.Add(this.lblConsumo);
+            this.Controls.Add(this.lblNCasa);
+            this.Controls.Add(this.txtConsumo);
+            this.Controls.Add(this.txtCasa);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.dgvLeituras);
+            this.Name = "FormConsumo";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLeituras)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvLeituras;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtCasa;
+        private System.Windows.Forms.TextBox txtConsumo;
+        private System.Windows.Forms.Label lblNCasa;
+        private System.Windows.Forms.Label lblConsumo;
+        private System.Windows.Forms.Button btnProcessar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
